f90fc60e82c4fe3b6214b6507d03e036affb32c2
노우제 <woojenoh@gmail.com>
2022-04-11 01:55:36 +0900

    Initial settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
-    siteLanguage: `en`,
+    siteTitle: `제노로그`,
+    siteTitleAlt: `제노로그`,
+    siteHeadline: `제노로그`,
+    siteUrl: `https://woojenoh.blog`,
+    siteDescription: `제노로그`,
+    siteLanguage: `ko`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `woojenoh`,
   },
   plugins: [
     {
@@ -33,29 +33,26 @@ module.exports = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
-          },
+            name: `Github`,
+            url: `https://github.com/woojenoh`,
+          }
         ],
+        formatString: 'YYYY.MM.DD',
       },
     },
     {
       resolve: `gatsby-omni-font-loader`,
       options: {
         enableListener: true,
-        preconnect: [`https://fonts.gstatic.com`],
+        preconnect: [`https://fonts.googleapis.com`],
         interval: 300,
         timeout: 30000,
         // If you plan on changing the font you'll also need to adjust the Theme UI config to edit the CSS
         // See: https://github.com/LekoArts/gatsby-themes/tree/main/examples/minimal-blog#changing-your-fonts
         web: [
           {
-            name: `IBM Plex Sans`,
-            file: `https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&display=swap`,
+            name: `Nanum Gothic`,
+            file: `https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap`,
           },
         ],
       },
@@ -64,27 +61,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `제노로그`,
+        short_name: `제노로그`,
+        description: `제노로그`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#6B46C1`,
         display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
+        icons: [],
       },
     },
     {
@@ -131,10 +117,16 @@ module.exports = {
               }
             `,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `제노로그`,
           },
         ],
       },
+    },
+    {
+      resolve: `gatsby-plugin-disqus`,
+      options: {
+          shortname: `woojenoh`
+      }
     },
     `gatsby-plugin-gatsby-cloud`,
     shouldAnalyseBundle && {
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,7 @@ import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 const NotFound = () => (
   <Layout>
     <h1>404 - Page Not Found</h1>
-    <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <p>페이지를 찾을 수 없습니다.</p>
   </Layout>
 )
 
